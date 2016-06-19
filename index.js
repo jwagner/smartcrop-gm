a52217fb7f145aab0dfcc0ba97bbc85a1ac2bb39
Jonas Wagner <jonas@29a.ch>
2016-06-19 13:12:18 +0200

    init

---
+++ index.js
@@ -0,0 +1,43 @@
+var smartcrop = require('smartcrop');
+var gm = require('gm');
+
+gm = gm.subClass({ imageMagick: true });
+
+var iop = {
+  open: function(src) {
+    var _gm = gm(src);
+    return new Promise(function(resolve, reject) {
+      _gm.size(function(err, size) {
+        if (err) return reject(err);
+        resolve({
+          width: size.width,
+          height: size.height,
+          _gm: _gm
+        });
+      });
+    });
+  },
+  resample: function(image, width, height) {
+    return Promise.resolve({
+      width: ~~width,
+      height: ~~height,
+      _gm: image._gm,
+    });
+  },
+  getData: function(image) {
+    return new Promise(function(resolve, reject) {
+      image._gm
+      .resize(image.width, image.height, '!')
+      .toBuffer('RGBA', function(err, buffer) {
+        if (err) return reject(err);
+        resolve(new smartcrop.ImgData(image.width, image.height, buffer));
+      });
+    });
+  },
+};
+
+exports.crop = function(img, options, callback) {
+  options = options || {};
+  options.imageOperations = iop;
+  return smartcrop.crop(img, options, callback);
+};
