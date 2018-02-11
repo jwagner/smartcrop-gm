e162e5a63009418d7cc011e33ba3a74d281e3eca
Jonas Wagner <jonas@29a.ch>
2018-02-11 22:55:42 +0100

    2.0.2

--- index.js
+++ index.js
@@ -21,19 +21,19 @@ var iop = {
     return Promise.resolve({
       width: ~~width,
       height: ~~height,
-      _gm: image._gm,
+      _gm: image._gm
     });
   },
   getData: function(image) {
     return new Promise(function(resolve, reject) {
       image._gm
-      .resize(image.width, image.height, '!')
-      .toBuffer('RGBA', function(err, buffer) {
-        if (err) return reject(err);
-        resolve(new smartcrop.ImgData(image.width, image.height, buffer));
-      });
+        .resize(image.width, image.height, '!')
+        .toBuffer('RGBA', function(err, buffer) {
+          if (err) return reject(err);
+          resolve(new smartcrop.ImgData(image.width, image.height, buffer));
+        });
     });
-  },
+  }
 };
 
 exports.crop = function(img, options, callback) {
